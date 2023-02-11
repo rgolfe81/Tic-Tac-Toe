0f546fee29bebde10982daeecfe90fb4a10a86e5
Ruben <rgolfe81@gmail.com>
2023-02-11 13:23:11 +0100

    Se genera un indicador de turno que cambia de valor con el turno y las fichas se incluyen del color de cada jugador

--- js/TableGame.js
+++ js/TableGame.js
@@ -7,13 +7,8 @@ let datosSesion = JSON.parse(sessionStorage.getItem("playersInfo"));
 
 let turn = true;
 
-let tokenP1 = 3;
-let tokenP2 = 3;
-
-let numMovesTotal = 0;
-let numMovesP1 = 0;
-let numMovesP2 = 0;
-
+let tokenP1 = 0;
+let tokenP2 = 0;
 
 let tablePresent = ["","","","","","","","",""];
 
@@ -26,7 +21,16 @@ let tableWin = [
     [2, 5, 8],
     [0, 4, 8],
     [2, 4, 6],
-  ];
+];
+
+let movesPlayer1 = document.getElementById("movesPlayer1");
+let movesPlayer2 = document.getElementById("movesPlayer2");
+
+let textTurnP1Js = document.getElementById("textTurnP1Html");
+let textTurnP2Js = document.getElementById("textTurnP2Html");
+textTurnP1Js.innerHTML= "Es tu turno";
+textTurnP2Js.innerHTML= "";
+
 
 const CheckWin = () => {
     console.log (tablePresent)
@@ -35,34 +39,27 @@ const CheckWin = () => {
 player1.innerHTML = `${datosSesion.player1}`;
 player2.innerHTML = `${datosSesion.player2}`;
 
-let movesPlayer1 = document.getElementById("movesPlayer1");
-let movePlusP1 = numMovesP1;
-let movesPlayer2 = document.getElementById("movesPlayer2");
-let movePlusP2 = numMovesP2;
-    
 
 table.map (cell => {
     cell.addEventListener("click", () => {
-        if ((cell.innerHTML === "") && (tokenP1>0 || tokenP2>0)){
+        if ((cell.innerHTML === "") && (tokenP1<3 || tokenP2<3)){
             if(turn){
                 cell.innerHTML = "X";
+                cell.style.color = "rgb(13, 110, 253)";
                 tablePresent[cell.id] = "X";
-                tokenP1--;
-                numMovesP1++;
-
-                movesPlayer1 = document.getElementById("movesPlayer1");
-                movePlusP1 = numMovesP1;
-                movesPlayer1.innerHTML = movePlusP1;
+                tokenP1++;
+                movesPlayer1.innerHTML = tokenP1;
+                textTurnP1Js.innerHTML= "";
+                textTurnP2Js.innerHTML= "Es tu turno";
             }
             else{
                 cell.innerHTML = "O";
+                cell.style.color = "rgb(220, 53, 69)"
                 tablePresent[cell.id] = "O";
-                tokenP2--;
-                numMovesP2++;
-
-                movesPlayer2 = document.getElementById("movesPlayer2");
-                movePlusP2 = numMovesP2;
-                movesPlayer2.innerHTML = movePlusP2;
+                tokenP2++;
+                movesPlayer2.innerHTML = tokenP2;
+                textTurnP1Js.innerHTML= "Es tu turno";
+                textTurnP2Js.innerHTML= "";
             }
             CheckWin();
             turn = !turn;
