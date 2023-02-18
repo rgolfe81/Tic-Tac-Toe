7048f25966e4e95f4b1e7e00ef79a86576634138
Ruben <rgolfe81@gmail.com>
2023-02-18 10:33:46 +0100

    Incluidos skins en TableGame modo desktop, y en TableWin modo smartphone y modo desktop

--- js/WinGame.js
+++ js/WinGame.js
@@ -6,6 +6,8 @@ let movesWinner = document.getElementById("movesWinner");
 let winnerSessionSymbol = sessionStorage.getItem("winnerSS");
 let winnerSessionName = sessionStorage.getItem("winnerNameSS");
 let winnerSessionMove = sessionStorage.getItem("winnerMoveSS");
+let playerWinner = document.getElementById("imgPlayerWinner");
+let createImgPlayerWinner;
 
 // Algoritmo *******************************************
 
@@ -13,9 +15,13 @@ if (winnerSessionSymbol === "X"){
     winner.innerHTML = winnerSessionName;
     movesWinner.innerHTML = winnerSessionMove;
     winner.style.color = "rgb(13, 110, 253)";
+    createImgPlayerWinner = '<img src="../img/Logo_Player1.png" alt="Imagen Player1" class="imgWin">';
+    playerWinner.innerHTML = createImgPlayerWinner;
 }
 if (winnerSessionSymbol === "O"){
     winner.innerHTML = winnerSessionName;
     movesWinner.innerHTML = winnerSessionMove;
     winner.style.color = "rgb(220, 53, 69)";
+    createImgPlayerWinner = '<img src="../img/Logo_Player2.png" alt="Imagen Player1" class="imgWin">';
+    playerWinner.innerHTML = createImgPlayerWinner;
 }
